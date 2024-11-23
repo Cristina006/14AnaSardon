18ba51352b1703afab054381ddaef5fd3081b0ca
OsCAR0367 <166433883+OsCAR0367@users.noreply.github.com>
2024-11-22 21:57:34 -0500

    Feat: Add class SimpleWidget

---
+++ app/src/main/java/com/lab/lab14sardon/SimpleWidget.kt
@@ -0,0 +1,8 @@
+package com.lab.lab14sardon
+
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+
+class SimpleWidget : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget = SimpleWidgetContent()
+}
